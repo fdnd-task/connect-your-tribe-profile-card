a6d7033c6a5565b2f73930da6de8a344fe268e2e
ju5tu5 <justus@ju5tu5.nl>
2023-02-02 18:41:18 +0100

    Example code finished

---
+++ index.js
@@ -0,0 +1,27 @@
+// Importeer express uit de node_modules map
+import express from 'express'
+
+// Maak een nieuwe express app aan
+const app = express()
+
+// Stel ejs in als template engine en geef de 'views' map door
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+// Gebruik de map 'public' voor statische resources
+app.use(express.static('public'))
+
+// Maak een route voor de index
+app.get('/', function (req, res) {
+  // res.send('Hello World!')
+  res.render('index')
+})
+
+// Stel het poortnummer in waar express op gaat luisteren
+app.set('port', process.env.PORT || 8000)
+
+// Start express op, haal het ingestelde poortnummer op
+app.listen(app.get('port'), function () {
+  // Toon een bericht in de console en geef het poortnummer door
+  console.log(`Application started on http://localhost:${app.get('port')}`)
+})
