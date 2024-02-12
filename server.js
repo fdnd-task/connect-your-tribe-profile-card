e9f9b1491ca5b892297a20db4667a09c334b4fb8
Cyd Stumpel <info@cydstumpel.nl>
2024-02-12 12:39:37 +0100

    Revert "Rewrite data variable, make view variable explicit"
    This reverts commit 7abd53d81f04dceecdbb237aeddbdca2d5489487.

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ import express from 'express'
 // Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
 import fetchJson from './helpers/fetch-json.js'
 
-// Haal data op uit de WHOIS API, ga pas verder als de data gedownload is
-const apiData = await fetchJson('https://fdnd.directus.app/items/person/69')
+// Haal data op uit de FDND API, ga pas verder als de data gedownload is
+const data = await fetchJson('https://fdnd.directus.app/items/person/65')
 // console.log(data); // uncomment om de opgehaalde data te checken
 
 // Maak een nieuwe express app aan
@@ -21,9 +21,8 @@ app.use(express.static('public'))
 
 // Maak een GET route voor de index
 app.get('/', function (request, response) {
-  const custom = JSON.parse(apiData.data.custom)
-  // Render index.ejs uit de views map en geef de opgehaalde data mee, in een variabele genaamd person
-  response.render('index', {person: apiData.data, customData: custom})
+  // Render index.ejs uit de views map en geef uit FDND API opgehaalde data mee
+  response.render('index', data)
 })
 
 // Maak een POST route voor de index
